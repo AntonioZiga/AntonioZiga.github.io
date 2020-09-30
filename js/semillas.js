df82dc71ad11071cff54c3597deed3469940c4a1
Antonio_Ziga <tony199605@gmail.com>
2020-09-29 22:45:25 -0500

    Correción de archivo semilla

--- js/semillas.js
+++ js/semillas.js
@@ -5,7 +5,7 @@ const proyectos = [
         descripcion: "Desarrollar una Plataforma para  la  Dirección de Educación Media superior que permita el control y captura de la información de los Bachilleratos del Estado de Oaxaca.",
         tecnologias: ["Laravel","Bootstrap","MySQL"],
         iconos : ["fab fa-laravel","fab fa-bootstrap","icon-mysql"],
-        banners : ["/proyecto_1/P1_S1.jpg","/proyecto_1/P1_S2.jpg","/proyecto_1/P1_S3.jpg"],       
+        banners : ["/proyecto_1/P1_S1.jpg","/proyecto_1/P1_S2.jpg","/proyecto_1/P1_S3.jpg", "/proyecto_1/P1_S4.jpg","/proyecto_1/P1_S5.png", "/proyecto_1/P1_S6.jpg"],       
         banner: "/proyecto_1/P1_S1.jpg",
         /*links : [{ id:"Demo", link:"",icono:"fas fa-laptop-code",text:"Ver Demo"},{ id:"Repositorio", link:"", icono:"fab fa-github",text:"Ver en GIT"}], */
         links : [],
