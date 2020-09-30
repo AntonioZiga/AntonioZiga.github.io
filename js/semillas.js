270cdcef695b1eba8ff1b9b0088b0fa0dcaad9a5
Antonio_Ziga <tony199605@gmail.com>
2020-09-29 22:33:49 -0500

    Correción ortografica, se agregaron mas imagenes del proyecto1 y se agregaron las redes sociales

--- js/semillas.js
+++ js/semillas.js
@@ -7,8 +7,10 @@ const proyectos = [
         iconos : ["fab fa-laravel","fab fa-bootstrap","icon-mysql"],
         banners : ["/proyecto_1/P1_S1.jpg","/proyecto_1/P1_S2.jpg","/proyecto_1/P1_S3.jpg"],       
         banner: "/proyecto_1/P1_S1.jpg",
-        links : [{ id:"Demo", link:"",icono:"fas fa-laptop-code",text:"Ver Demo"},{ id:"Repositorio", link:"", icono:"fab fa-github",text:"Ver en GIT"}],
-    },  {
+        /*links : [{ id:"Demo", link:"",icono:"fas fa-laptop-code",text:"Ver Demo"},{ id:"Repositorio", link:"", icono:"fab fa-github",text:"Ver en GIT"}], */
+        links : [],
+    }  
+    /*{
         id: "2",
         nombre: "Plataforma única de seguimiento y evaluación educativa de los Bachilleratos del Estado de Oaxaca",
         descripcion: "Desarrollar una Plataforma para  la  Dirección de Educación Media superior que permita el control y captura de la información de los Bachilleratos del Estado de Oaxaca.",
@@ -17,5 +19,5 @@ const proyectos = [
         banners : ["/proyecto_1/P1_S1.jpg"],       
         banner: "/proyecto_1/P1_S1.jpg",
         links : [],
-    }
+    } */
 ];
